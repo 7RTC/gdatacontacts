7ade93478a46e589c2396f2909c4858e53d68a19
Anthony Accioly <a.accioly@7rtc.com>
2012-07-29 14:04:12 -0300

    Suporte a operadoras no Label

--- src/com/sevenrtc/gdata/contacts/GoogleContactsTest.java
+++ src/com/sevenrtc/gdata/contacts/GoogleContactsTest.java
@@ -1,5 +1,7 @@
 package com.sevenrtc.gdata.contacts;
 
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableSet;
 import com.google.gdata.client.Query;
 import com.google.gdata.client.contacts.ContactsService;
 import com.google.gdata.data.contacts.ContactEntry;
@@ -25,48 +27,69 @@ import javax.swing.JOptionPane;
  */
 public class GoogleContactsTest {
     
-    private static final Pattern mobilesCodeArea11 = Pattern.compile("^(:?0(:?\\d{2})?11)?+([5-9])\\d{7}$");
-    private static Set<String> celulares;
-    private static Set<String> naoCelulares;
-   
+    private final ContactsService contactService;
+    private final Set<String> labelsToIgnore;
+    private final Set<String> mnos;
+    private final Pattern mobilesCodeArea11;
+    private final String username;
+    
+    private final Set<String> updated;
+    private final Set<String> skipped;
+
+
+    public GoogleContactsTest() throws AuthenticationException, ServiceException, IOException {
+        final Properties properties = new Properties();
+        properties.load(GoogleContactsTest.class.getResourceAsStream("./Credentials.properties"));
+        
+        mobilesCodeArea11 = Pattern.compile("^(:?0(:?\\d{2})?11)?+([5-9])\\d{7}$");
+        
+        final String[] mnosProperty = properties.getProperty("mnos").split(";");
+        mnos = ImmutableSet.copyOf(mnosProperty);
+        
+        final String[] ignoreListProperty = properties.getProperty("ignoreList").split(";");
+        labelsToIgnore = ImmutableSet.copyOf(ignoreListProperty);
+        
+        username = properties.getProperty("username");
+        
+        contactService = new ContactsService("7rtc-cellPhoneDigits");
+        contactService.setUserCredentials(username, properties.getProperty("password"));
+        
+        updated = new TreeSet<>();
+        skipped = new TreeSet<>();
+        
+    }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws AuthenticationException, ServiceException, IOException {
-        celulares = new TreeSet<>();
-        naoCelulares = new TreeSet<>();
-        final Properties properties = new Properties();
-        properties.load(GoogleContactsTest.class.getResourceAsStream("./Credentials.properties"));
+    public static void main(String[] args) throws AuthenticationException, ServiceException, IOException  {
         
-        final String username = properties.getProperty("username");
-        final ContactsService myService = new ContactsService("7rtc-cellPhoneDigits");
-        myService.setUserCredentials(username, properties.getProperty("password"));
-        analyzeContacts(myService, username);
-        //printAllGroups(myService);
+        GoogleContactsTest gct = new GoogleContactsTest();
+        gct.analyzeContacts();
+        //gct.printAllGroups();
     }
     
-    public static void analyzeContacts(ContactsService myService, String username)
-            throws ServiceException, IOException {
+    private void analyzeContacts() throws ServiceException, IOException {
         
-        // Create query and submit a request
+        // Query Google concats
         final URL feedUrl = new URL("https://www.google.com/m8/feeds/contacts/" + username + "/full");
         final Query myQuery = new Query(feedUrl);
         myQuery.setMaxResults(Integer.MAX_VALUE);
+        // Comment the next line to use all groups instead of My Contacts
         myQuery.setStringCustomParameter("group", "http://www.google.com/m8/feeds/groups/" + username + "/base/6");
-        final ContactFeed resultFeed = myService.query(myQuery, ContactFeed.class);
+        final ContactFeed resultFeed = contactService.query(myQuery, ContactFeed.class);
 
         final int size = resultFeed.getTotalResults();
         
         for (ContactEntry contact : resultFeed.getEntries()) {
-            verifyPhoneNumbers(myService, contact);
+            verifyPhoneNumbers(contact);
         }
         
         System.out.println("Number of contacts:  " + size);
         
     }
     
-    public static ContactEntry verifyPhoneNumbers(ContactsService myService, ContactEntry entry)
+    public ContactEntry verifyPhoneNumbers(ContactEntry entry)
             throws ServiceException, IOException {
         
         final String entryName = entry.getTitle().getPlainText();
@@ -75,6 +98,7 @@ public class GoogleContactsTest {
         for (PhoneNumber phoneNumber : entry.getPhoneNumbers()) {
             
             final String originalNumber = phoneNumber.getPhoneNumber();
+            final String label = phoneNumber.getLabel();
             
             // Check if the number is a mobile with no area code or area code 11
             final Matcher m = mobilesCodeArea11.matcher(originalNumber);
@@ -85,58 +109,76 @@ public class GoogleContactsTest {
                 final String withDigit = new StringBuilder(originalNumber)
                         .insert(firstDigitPosition, 9).toString();
                 
-                // If the number starts with 7, 8 or 9 it is a mobile
-                if (firstDigit > 6) {
-                    celulares.add(entryName + ": " + originalNumber + " - " + withDigit + " automático");
+                // If the number starts with 8 or 9 it is a mobile
+                if (firstDigit > 7) {
+                    updated.add(entryName + ": " + originalNumber + " - " + withDigit + " automático");
                     phoneNumber.setPhoneNumber(withDigit);
                     mustUpdate = true;
                 }
-                // If the number starts with 5 or 6, ask the user for confirmation
+                // If the number starts with 5, 6 or 7 
                 else {
-                    final int option = JOptionPane.showConfirmDialog(null, 
-                            String.format("É impossível determinar se o número %s,\n"
-                            + "pertencente ao contato %s, é um celular.\n"
-                            + "Inserir o dígito assim mesmo?\n\n"
-                            + "Número após modificação: %s", originalNumber, entryName, withDigit), 
-                            "Inserir digito?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-                    if (option == JOptionPane.YES_OPTION) {
-                        celulares.add(entryName + ": " + originalNumber + " - " + withDigit + " manual");
+                    boolean hasLabel = !Strings.isNullOrEmpty(label);
+                    // Try to detect the mno from the label
+                    if (hasLabel && mnos.contains(label)) {
+                        updated.add(entryName + ": " + originalNumber + " - " + withDigit + " automático");
                         phoneNumber.setPhoneNumber(withDigit);
                         mustUpdate = true;
-                    } else {
-                        naoCelulares.add(entryName + ": " + originalNumber + " manual");
+                    } 
+                    // Skip labels from non mobile operators
+                    else if (hasLabel && labelsToIgnore.contains(label)) {
+                        skipped.add(entryName + ": " + originalNumber + " automático");
+                    }
+                    // Else ak the user
+                    else {
+                        if (hasLabel) {
+                            System.out.println(label + " : " +  originalNumber);
+                        }
+                        final int option = JOptionPane.showConfirmDialog(null,
+                                String.format("É impossível determinar se o número %s,\n"
+                                + "pertencente ao contato %s, é um celular.\n"
+                                + "Inserir o dígito assim mesmo?\n\n"
+                                + "Número após modificação: %s", originalNumber, entryName, withDigit),
+                                "Inserir digito?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                        if (option == JOptionPane.YES_OPTION) {
+                            updated.add(entryName + ": " + originalNumber + " - " + withDigit + " manual");
+                            phoneNumber.setPhoneNumber(withDigit);
+                            mustUpdate = true;
+                        } else {
+                            skipped.add(entryName + ": " + originalNumber + " manual");
+                        }
                     }
                 }
             } else {
-                naoCelulares.add(entryName + ": " + originalNumber + " automático");
+                skipped.add(entryName + ": " + originalNumber + " automático");
             }        
                 
         }
         
         if (mustUpdate) {
             final URL entryUrl = new URL(entry.getSelfLink().getHref());
-            final ContactEntry originalEntry = myService.getEntry(entryUrl, ContactEntry.class);
-            System.out.println("\nUpdating " + entryName);
-            System.out.println("-----------------------------------");
-            System.out.printf("|%-15s | %-15s|\n", "BEFORE", "AFTER");
-            System.out.println("-----------------------------------");
+            final ContactEntry originalEntry = contactService.getEntry(entryUrl, ContactEntry.class);
+            System.out.println("\nAtualizando " + entryName);
+            System.out.println("+---------------------------------+");
+            System.out.printf("|%-15s | %-15s|\n", "ANTES", "DEPOIS");
+            System.out.println("+---------------------------------+");
             for (int i = 0; i < originalEntry.getPhoneNumbers().size(); i++) {
                 PhoneNumber before = originalEntry.getPhoneNumbers().get(i);
                 PhoneNumber after = entry.getPhoneNumbers().get(i);
                 System.out.printf("|%-15s | %-15s|\n", before.getPhoneNumber(), after.getPhoneNumber());
             }
-            System.out.println("-----------------------------------");
+            System.out.println("+---------------------------------+");
             final URL editUrl = new URL(entry.getEditLink().getHref());
-            /*entry = myService.update(editUrl, entry);*/
+            // Comment the next line to avoid updating your contact list over the first run
+            entry = contactService.update(editUrl, entry);
         }
         return entry;
     }
     
-    public static void printAllGroups(ContactsService myService)
+    private void printAllGroups()
             throws ServiceException, IOException {
         // Request the feed
-        URL feedUrl = new URL("https://www.google.com/m8/feeds/groups/a.accioly@7rtc.com/full");
-        ContactGroupFeed resultFeed = myService.getFeed(feedUrl, ContactGroupFeed.class);
+        URL feedUrl = new URL("https://www.google.com/m8/feeds/groups/" + username + "/full");
+        ContactGroupFeed resultFeed = contactService.getFeed(feedUrl, ContactGroupFeed.class);
         // Print the results
         System.out.println(resultFeed.getTitle().getPlainText());
 
